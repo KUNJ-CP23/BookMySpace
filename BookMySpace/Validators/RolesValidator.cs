01d119ad95c84174f941eb7fd1d7387dbd0001f5
Kunj Ramoliya <kunjramoliya367@gmail.com>
2026-02-02 08:38:41 +0530

    role

--- BookMySpace/Validators/RolesValidator.cs
+++ BookMySpace/Validators/RolesValidator.cs
@@ -7,7 +7,7 @@ public class RolesValidator : AbstractValidator<AddUpdateRoleDTO>
     {
         RuleFor(x => x.RoleName)
             .NotEmpty().WithMessage("Role name is required")
-            .MinimumLength(5).WithMessage("Role name must contain at least 1 character")
+            .MinimumLength(2).WithMessage("Role name must contain at least 1 character")
             .MaximumLength(50).WithMessage("Role name cannot exceed 50 characters");
     }
 }
