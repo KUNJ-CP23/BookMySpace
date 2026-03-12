4ce660127c8e912fa6af5bdbd8c6dcc45e19ebd4
Kunj Ramoliya <kunjramoliya367@gmail.com>
2026-03-12 13:08:50 +0530

    chnaged

--- bookmyspace-frontend/src/App.jsx
+++ bookmyspace-frontend/src/App.jsx
@@ -3,11 +3,19 @@ import DashboardLayout from "./components/DashboardLayout";
 
 import Home from "./pages/Home";
 import Users from "./pages/Users";
+import UserForm from "./pages/UserForm";
 import Facilities from "./pages/Facilities";
+import FacilityForm from "./pages/FacilityForm";
+import FacilityDetail from "./pages/FacilityDetail";
 import Bookings from "./pages/Bookings";
+import BookingForm from "./pages/BookingForm";
+import BookingDetail from "./pages/BookingDetail";
 import Payment from "./pages/Payment";
+import PaymentForm from "./pages/PaymentForm";
 import Roles from "./pages/Roles";
 import Reviews from "./pages/Reviews";
+import ReviewForm from "./pages/ReviewForm";
+import ReviewDetail from "./pages/ReviewDetail";
 import Documents from "./pages/Documents";
 import FacilityImages from "./pages/FacilityImages";
 import Login from "./pages/Login";
@@ -48,20 +56,52 @@ export default function App() {
           <Route index element={<Home />} />
 
           <Route path="users" element={RoleRoute(["Admin"], <Users />)} />
+          <Route path="users/form" element={RoleRoute(["Admin"], <UserForm />)} />
+          <Route path="users/form/:id" element={RoleRoute(["Admin"], <UserForm />)} />
           <Route path="roles" element={RoleRoute(["Admin"], <Roles />)} />
           <Route path="payment" element={RoleRoute(["Admin"], <Payment />)} />
+          <Route path="payments" element={RoleRoute(["Admin"], <Payment />)} />
+          <Route path="payments/form" element={RoleRoute(["Admin"], <PaymentForm />)} />
+          <Route path="payments/form/:id" element={RoleRoute(["Admin"], <PaymentForm />)} />
 
           <Route
             path="facilities"
             element={RoleRoute(["Admin", "Owner", "Customer"], <Facilities />)}
+          />
+          <Route
+            path="facilities/form"
+            element={RoleRoute(["Admin", "Owner"], <FacilityForm />)}
+          />
+          <Route
+            path="facilities/form/:id"
+            element={RoleRoute(["Admin", "Owner"], <FacilityForm />)}
+          />
+          <Route
+            path="facilities/:id"
+            element={RoleRoute(["Admin", "Owner", "Customer"], <FacilityDetail />)}
           />
 
           <Route
             path="bookings"
             element={RoleRoute(["Admin", "Customer"], <Bookings />)}
           />
+          <Route
+            path="bookings/form"
+            element={RoleRoute(["Admin"], <BookingForm />)}
+          />
+          <Route
+            path="bookings/form/:id"
+            element={RoleRoute(["Admin"], <BookingForm />)}
+          />
+          <Route
+            path="bookings/:id"
+            element={RoleRoute(["Admin", "Customer"], <BookingDetail />)}
+          />
 
           <Route path="reviews" element={<Reviews />} />
+          <Route path="reviews/form" element={<ReviewForm />} />
+          <Route path="reviews/form/:id" element={<ReviewForm />} />
+          <Route path="reviews/:id" element={<ReviewDetail />} />
           <Route path="documents" element={<Documents />} />
           <Route path="facility-images" element={<FacilityImages />} />
         </Route>
